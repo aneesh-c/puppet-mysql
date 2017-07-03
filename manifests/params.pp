a6de213863cf2e85a23cd6a32823b0d1e88aa9cb
Aneesh C <admn.nanonet@gmail.com>
2017-07-03 12:06:01 +0530

    Release 4.0.0

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,22 +4,40 @@ class mysql::params {
     $configfile = '/etc/my.cnf'
   }
   elsif $::osfamily == 'Debian' {
-    case $::operatingsystemrelease {
-      /^12.*/: {
-        $packagename_default = [ 'mysql-server', 'libapache2-mod-auth-mysql' ]
-        $configfile = '/etc/mysql/my.cnf'
+    if $::operatingsystem == 'Ubuntu' {
+      case $::operatingsystemrelease {
+        /^12.*/: {
+          $packagename_default = [ 'mysql-server', 'libapache2-mod-auth-mysql' ]
+          $configfile = '/etc/mysql/my.cnf'
+        }
+        /^14.*/: {
+          $packagename_default = [ 'mysql-server', 'libapache2-mod-auth-mysql' ]
+          $configfile = '/etc/mysql/my.cnf'
+        }
+        /^16.*/: {
+          $packagename_default = [ 'mysql-server' ]
+          $configfile = '/etc/mysql/mysql.conf.d/mysqld.cnf'
+        }
+        default: {
+          $packagename_default = [ 'mysql-server', 'libapache2-mod-auth-mysql' ]
+          $configfile = '/etc/mysql/my.cnf'
+        }
       }
-      /^14.*/: {
-        $packagename_default = [ 'mysql-server', 'libapache2-mod-auth-mysql' ]
-        $configfile = '/etc/mysql/my.cnf'
-      }
-      /^16.*/: {
-        $packagename_default = [ 'mysql-server' ]
-        $configfile = '/etc/mysql/mysql.conf.d/mysqld.cnf'
-      }
-      default: {
-        $packagename_default = [ 'mysql-server', 'libapache2-mod-auth-mysql' ]
-        $configfile = '/etc/mysql/my.cnf'
+    }
+    elsif $::operatingsystem == 'Debian' {
+      case $::operatingsystemrelease {
+        /^7.*/: {
+          $packagename_default = [ 'mysql-server', 'libapache2-mod-auth-mysql' ]
+          $configfile = '/etc/mysql/my.cnf'
+        }
+        /^8.*/: {
+          $packagename_default = [ 'mysql-server' ]
+          $configfile = '/etc/mysql/my.cnf'
+        }
+        default: {
+          $packagename_default = [ 'mysql-server', 'libapache2-mod-auth-mysql' ]
+          $configfile = '/etc/mysql/my.cnf'
+        }
       }
     }
   }
